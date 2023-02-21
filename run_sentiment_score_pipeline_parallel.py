fc569ce74590a48507533528137c0fae0c7057c8
Yuanzhi Wang <89885731+yuanzwang5@users.noreply.github.com>
2023-02-21 19:57:11 +0800

    Create run_sentiment_score_pipeline_parallel.py

---
+++ run_sentiment_score_pipeline_parallel.py
@@ -0,0 +1,71 @@
+import sys
+sys.path.insert(0, '/mnt/utnfs/data/sentiment_score_pipeline/python_code/v0.1.0')
+from get_data_from_mysql import get_data_from_mysql
+from generate_bond_profile_features import generate_bond_profile_features
+from generate_origin_news import generate_origin_news
+from generate_bond_related_news import generate_bond_related_news
+from generate_default_labels import generate_default_labels
+from generate_news_jiebascore import generate_news_jiebascore
+from generate_panel_table import generate_panel_table
+from generate_model_output import generate_model_output
+import pandas as pd
+
+df_daterange = pd.DataFrame(pd.date_range('2019-03-15','2022-07-15',freq='M'), columns=['Monthend'])
+df_daterange['update_date'] = df_daterange['Monthend'].apply(lambda x: str(x)[:7]+'-15')
+datelist = list(df_daterange['update_date'].unique())
+
+version = 'v0.1.0'
+
+def main(data_date, version):
+    # update_data = True
+    
+    # ### update the data from mySQLDB
+    # if update_data == True:
+    #     get_data_from_mysql.get_updated_data_from_db(data_date=data_date)
+    
+    # ### Step 1: generate bond profile
+    # generate_bond_profile_features.generate_bond_profile_features(data_date=data_date)
+    # print('-----------------------------------------------')
+    # print('Finish generate bond profile')
+    # print('-----------------------------------------------')
+    
+    # ### Step 2: generate original news
+    # generate_origin_news.generate_origin_news(data_date=data_date)
+    # print('-----------------------------------------------')
+    # print('Finish generate original news')
+    # print('-----------------------------------------------')
+    
+    # ### Step 3: generate bond related news
+    # generate_bond_related_news.generate_bond_related_news(data_date=data_date)
+    # print('-----------------------------------------------')
+    # print('Finish generate bond related news')
+    # print('-----------------------------------------------')
+    
+    # ### Step 4: generate default labels
+    # generate_default_labels.generate_default_labels(data_date=data_date)
+    # print('-----------------------------------------------')
+    # print('Finish generate default labels')
+    # print('-----------------------------------------------')
+    
+    # ### Step 5: generate news jieba score
+    # generate_news_jiebascore.generate_news_jiebascore(data_date=data_date)
+    # print('-----------------------------------------------')
+    # print('Finish generate news jieba score')
+    # print('-----------------------------------------------')
+    
+    # # ### Step 6: generate panel table
+    # generate_panel_table.generate_panel_table(data_date=data_date)
+    # print('-----------------------------------------------')
+    # print('Finish generate panel table')
+    # print('-----------------------------------------------')
+
+    ### Step 7: generate model output
+    generate_model_output.generate_model_output(data_date=data_date, version=version)
+    print('-----------------------------------------------')
+    print('Finish generate model output')
+    print('-----------------------------------------------')
+
+for data_date in datelist:
+    print('-----------------------------------------------')
+    print(data_date, ' start to product.')
+    main(data_date, version)
